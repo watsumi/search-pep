dc3fdfbd3a5b3aaaae0c3fadec219eb60b8622ac
watsumi <w.utsumi2@gmail.com>
2021-02-22 14:04:38 +0900

    [Add]tailwindcssの導入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h1 class="text-4xl text-green-700 text-center font-semibold">Hello Tailwind</h1>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
