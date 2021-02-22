81290241f3a1542fb24e6858520dc0ba3f668934
watsumi <w.utsumi2@gmail.com>
2021-02-22 22:48:42 +0900

    [Fix]fabiconの差し替え

---
+++ src/components/CorrectModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="modal"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/QuestionItem.vue
+++ src/components/QuestionItem.vue
@@ -3,9 +3,7 @@
       :id="'q-'+question.id"
       class="float-left md:float-left"
     >
-      <span class="h-12 w-12">
-        <img class='hover:opacity-50 container mx-auto h-20 w-20' :src="question.img">
-      </span>
+      <img class='hover:opacity-50 container mx-auto h-20 w-20' :src="question.img">
     </div>
 </template>
 
