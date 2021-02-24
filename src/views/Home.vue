0324bbcd06e9bdac13eccf6634fa966e40a11d7c
watsumi <w.utsumi2@gmail.com>
2021-02-24 10:04:24 +0900

    SPAに対応

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,36 @@
 <template>
   <div>
-    <div class="flex justify-center mt-16">
-      <Logo />
-    </div>
-    <div class="flex justify-center mt-16">
-      <Pep />
-    </div>
-    <div class="flex justify-center mt-16">
-      <div class="hover:opacity-50">
-        <Start  @click.native="reload"/>
-      </div>
-    </div>
+    <TopPage 
+      v-if="isVisibleTopPage"
+      @click.native="handleCloseTopPage"
+    />
+    <Question 
+      v-if="isVisibleQuestion"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Logo from '../components/images/Logo'
-import Pep from '../components/images/Pep'
-import Start from '../components/images/Start'
+import TopPage from './TopPage.vue'
+import Question from './Question.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      isVisibleTopPage: true, 
+      isVisibleQuestion: false, 
+    }
+  },
   components: {
-    Logo,
-    Pep,
-    Start
+    TopPage,
+    Question
   },
   methods: {
-    reload() {
-      this.$router.go({path: this.$router.push('/question'), force: true});
+    handleCloseTopPage(){
+      this.isVisibleTopPage = false;
+      this.isVisibleQuestion = true;
     },
   }
 }
